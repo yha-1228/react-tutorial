2f5163c0ab961737f1ab6840f05a6734e74b0a5e
長谷川 裕大 <gssbtsm999@gmail.com>
2021-01-09 23:21:04 +0900

    U

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -14,17 +14,17 @@ const Board: React.FC<Props> = ({ squares, onSquareClick }) => {
 
   return (
     <>
-      <div className="board-row">
+      <div className="Board__row">
         {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
       </div>
-      <div className="board-row">
+      <div className="Board__row">
         {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
       </div>
-      <div className="board-row">
+      <div className="Board__row">
         {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
