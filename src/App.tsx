40c34e493570725163f121500159df4464814433
長谷川 裕大 <gssbtsm999@gmail.com>
2021-01-09 23:26:11 +0900

    U

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import Game from './components/Game';
 import './App.css';
 
-const App: React.FC = () => {
-  return <Game />;
-};
+const App: React.FC = () => <Game />;
 
 export default App;
